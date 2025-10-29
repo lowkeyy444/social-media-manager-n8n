a7ee5a941b4a8f7e4d1643c68fc1981218f15f32
Dawn Varghese <dawnvarghese@Dawns-MacBook-Air.local>
2025-10-29 13:39:52 +0530

    before redoing single post

--- src/config/n8n.js
+++ src/config/n8n.js
@@ -3,8 +3,10 @@
 export const N8N_WEBHOOKS = {
   withLogoSingle: "https://lately-boss-gator.ngrok-free.app/webhook-test/linkdin-post-start-withurl",
   withoutLogoSingle: "https://lately-boss-gator.ngrok-free.app/webhook/linkdin-post-start",
-  customApi: "https://lately-boss-gator.ngrok-free.app/webhook/linkdin-post-start-multiplepost-loopfn"
+  customApi: "https://lately-boss-gator.ngrok-free.app/webhook/linkdin-post-start-multiplepost-loopfn" //multipostWithoutLogo
+  //multiplepostwithurl
+
 };
 
-
 // customApi: "https://lately-boss-gator.ngrok-free.app/webhook-test/linkdin-post-start-multiplepost-loopfn"
+
